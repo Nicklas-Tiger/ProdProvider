fb61eaaac8f2e3d894fa0fb921134174896aeb1d
Nicklas Tiger <nicklas.tiger@gmail.com>
2024-11-11 11:30:05 +0100

    product service created

---
+++ WebApi/Services/ProductService.cs
@@ -0,0 +1,26 @@
+﻿using WebApi.Factories;
+using WebApi.Models;
+
+namespace WebApi.Services;
+
+public class ProductService //(DataContext context)
+
+{
+    //private readonly DataContext _context = context;
+
+    public async  Task<ResponseResult> Create(ProductRegistrationForm form)
+    {
+        var entity = ProductFactory.Create(form);
+
+        //if (await _context.Products.AnyAsync(x => x.Title == entity.Title)) 
+        //    return ResponseResultFactory.Exists<ResponseResult>();
+        // _context.Products.Add(entity);
+        // await _context.SaveChangesAsync();
+
+        var product = ProductFactory.Create(entity);
+        var result = ResponseResultFactory.Success<Product>(data: product);
+
+        return result;
+
+    }
+}
