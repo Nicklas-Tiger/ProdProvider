830b4f41e7efcc44e0118988c2bf2eba1bc78737
Nicklas Tiger <nicklas.tiger@gmail.com>
2024-11-11 11:40:55 +0100

    interface for responseresult

---
+++ WebApi/Interfaces/IResponseResult.cs
@@ -0,0 +1,21 @@
+﻿namespace WebApi.Interfaces;
+
+public interface IResponseResult : IBaseResponseResult
+{
+
+}
+
+public interface IResponseResult<T> : IResponseResult
+{
+    public T? Data { get; set; }
+}
+
+
+public interface IBaseResponseResult
+{
+    public bool Success { get; set; }
+
+    public int StatusCode { get; set; }
+
+    public string? Message { get; set; }
+}
--- WebApi/Models/ResponseResult.cs
+++ WebApi/Models/ResponseResult.cs
@@ -1,17 +1,22 @@
-﻿namespace WebApi.Models;
+﻿using WebApi.Interfaces;
 
-public class ResponseResult : BaseResponseResult
+namespace WebApi.Models;
+
+public class ResponseResult : BaseResponseResult, IResponseResult
 {
+
 }
-public class ResponseResult<T> : BaseResponseResult
+
+public class ResponseResult<T> : BaseResponseResult, IResponseResult<T>
 {
     public T? Data { get; set; }
 }
 
-public abstract class BaseResponseResult
+public abstract class BaseResponseResult : IBaseResponseResult
 {
     public bool Success { get; set; }
-    public int StatusCode { get; set; }
-    public string? Message { get; set; }
 
+    public int StatusCode { get; set; }
+
+    public string? Message { get; set; }
 }
