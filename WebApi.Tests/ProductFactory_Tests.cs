01c848a0ef23a51aa28081bc9427faafbef1ca70
Nicklas Tiger <nicklas.tiger@gmail.com>
2024-11-11 10:56:28 +0100

    Finished tests for Factories

---
+++ WebApi.Tests/ProductFactory_Tests.cs
@@ -0,0 +1,43 @@
+﻿using System.Runtime.InteropServices;
+using WebApi.Factories;
+using WebApi.Models;
+
+namespace WebApi.Tests;
+
+public class ProductFactory_Tests
+{
+    private ProductRegistrationForm _formWithData = new ProductRegistrationForm
+    {
+        Title = "Test Product",
+        Price = "200"
+    };
+    private ProductEntity _entityWithData = new ProductEntity
+    {
+        Id = 1,
+        Title = "Test product",
+        Price = 200
+    };
+
+    [Fact]
+    public void Create_ShouldReturnProductEntity_FromProductRegistrationForm()
+    {
+
+        // Act
+        var result = ProductFactory.Create(_formWithData);
+
+        // Assert
+        Assert.IsType<ProductEntity>(result);
+    }
+
+    [Fact]
+    public void Create_ShouldReturnProduct_FromProductEntity()
+    {
+
+        // Act
+        var result = ProductFactory.Create(_entityWithData);
+
+        // Assert
+        Assert.IsType<Product>(result);
+    }
+}
+
