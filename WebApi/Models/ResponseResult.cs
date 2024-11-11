07e5fc90747070d6fe89bb7e629214e384fd2196
Nicklas Tiger <nicklas.tiger@gmail.com>
2024-11-11 10:21:29 +0100

    Models and factories done

---
+++ WebApi/Models/ResponseResult.cs
@@ -0,0 +1,17 @@
+﻿namespace WebApi.Models;
+
+public class ResponseResult : BaseResponseResult
+{
+}
+public class ResponseResult<T> : BaseResponseResult
+{
+    public T? Data { get; set; }
+}
+
+public abstract class BaseResponseResult
+{
+    public bool Success { get; set; }
+    public int StatusCode { get; set; }
+    public string? Message { get; set; }
+
+}
